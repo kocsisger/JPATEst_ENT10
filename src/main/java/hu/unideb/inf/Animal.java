2f2ab62f5f31f811918874eef2bbd3e10e9e1dd2
John Smith <you@inf.unideb.hu>
2024-10-08 11:17:10 +0200

    Animal entity added

--- src/main/java/hu/unideb/inf/Animal.java
+++ src/main/java/hu/unideb/inf/Animal.java
@@ -1,6 +1,13 @@
 package hu.unideb.inf;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Animal {
+    @Id
+    @GeneratedValue
     private int id;
     private String name;
     private int age;
@@ -10,4 +17,33 @@ public class Animal {
         MALE,FEMALE,UNKNOWN;
     }
 
+    public Animal(String name, int age, GenderType gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public GenderType getGender() {
+        return gender;
+    }
+
+    public void setGender(GenderType gender) {
+        this.gender = gender;
+    }
 }
